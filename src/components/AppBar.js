288738317da4b92799c74255cb444fd7e5d490ca
PLCeth <dev@bulltrader.io>
2022-05-16 10:35:50 -0500

    pushing

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -15,22 +15,15 @@ const pages = ['Home', 'About Us', 'Work', "Read", "Listen", "Invest", "New Memb
 
 export const MainAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <AppBar position="static" className="appBar">
