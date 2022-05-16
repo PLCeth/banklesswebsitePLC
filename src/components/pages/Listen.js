82208e689c9a86b78f6a82c169604666fe297968
PLCeth <dev@bulltrader.io>
2022-05-16 16:55:11 -0500

    listen pg

--- src/components/pages/Listen.js
+++ src/components/pages/Listen.js
@@ -35,20 +35,33 @@ export const Listen = (props) => {
 
                     <Grid item xs={12} md={6} style={{padding: '25px', marginBottom: '20px'}}>
                         <div className="graybox">
-                        <h1 style={{padding: '10px'}}>AB INITIO: FROM THE BEGINNING</h1>
+                        <h1 style={{padding: '10px', textAlign: 'center'}}>AB INITIO: FROM THE BEGINNING</h1>
                         <div>Hosted By </div>
                         <p style={{lineHeight: '1.5em'}}>A Legal Guild podcast series with each episode focusing on addressing current DAO and crypto-related legal issues via an interview with a subject matter expert.</p>
                         </div>
                     </Grid> 
                     <Grid item xs={12} md={6} style={{padding: '25px'}}>
                         <div className='graybox'>
-                        <h1 style={{padding: '10px'}}>BOUNTY HUNTER</h1>
+                        <h1 style={{padding: '10px', textAlign: 'center'}}>BOUNTY HUNTER</h1>
                         <div>Hosted By </div>
                         <p style={{lineHeight: '1.5em'}}>Bounty Hunter is a show about how to find engaging and rewarding work in the new Web3 economy. Join serial entrepreneur Brandon Nolte as he interviews community builders, DAO contributors, and the best innovators in Web3. Follow Bounty Hunter on  Twitter.</p>
                         </div>
                     </Grid> 
+
+      
                 </Grid>
             </Container>
+
+            <div className='elipBG'>
+                <Container>
+                <Grid container>
+                    <Grid item xs={12} md={6} style={{padding: '20px'}} >
+                        <h1 style={{fontSize: '4em'}}>Have your own podcast idea?</h1>
+                        <p style={{lineHeight: '1.5em', fontSize: '1.7em'}}>Join <a href="https://discord.com/invite/bankless" target="_blank" rel="noreferrer" className='destyle' style={{color: '#F00'}}>BanklessDAO</a> and get in contact with the Podcast Hatchery.</p>
+                    </Grid>
+                </Grid>
+            </Container>
+            </div>
         </div>
     )
 }
