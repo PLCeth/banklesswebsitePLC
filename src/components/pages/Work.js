d75a0099a887e30cfcb2632644b82b725d9eeb16
PLCeth <dev@bulltrader.io>
2022-05-17 13:50:05 -0500

    updating

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,20 +1,10 @@
-import { Container, Grid, Tabs, Typography, Tab, Box } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 import * as React from 'react'
 import { Fade } from 'react-reveal';
 import { ContentSlider } from '../ContentSlider';
 import { slidesOne, slidesTwo, slidesThree, slidesFour } from '../workSlides';
 
 export const Work = (props) => {
-    const [valueThree, setValueTabThree] = React.useState(1);
-    const [valueFour, setValueTabFour] = React.useState(1);
-
-
-    const handleChangeThree  = (event, newValue) => {
-        setValueTabThree(newValue);
-    };
-    const handleChangeFour  = (event, newValue) => {
-        setValueTabFour(newValue);
-    };
 
     return (
         <div style={{minHeight: '100vh'}}>
@@ -88,33 +78,3 @@ export const Work = (props) => {
     )
 }
 
-
-
-const TabPanel = (props) => {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography >{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-  
