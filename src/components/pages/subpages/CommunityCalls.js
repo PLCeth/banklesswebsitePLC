64310eb30fb8b92fd85b2afd584d60b72bdd8c43
PLCeth <dev@bulltrader.io>
2022-05-17 17:29:56 -0500

    community calls pg

--- src/components/pages/subpages/CommunityCalls.js
+++ src/components/pages/subpages/CommunityCalls.js
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material'
+import { Container, Grid, Button } from '@mui/material'
 import * as React from 'react'
 
 
@@ -6,16 +6,87 @@ export const CommunityCalls = (props) => {
 
 
     return (
-        <div>
+        <Container style={{paddingBottom: '150px'}}>
+
             <Grid container style={{justifyContent: 'center'}}>
                 <Grid item xs={12} md={6}>
                     <h1 style={{textAlign: 'center'}}>Community Calls</h1>
                     <div className='divider' style={{maxWidth: '500px', marginTop: '0'}}></div>
                     <p className='centered textWreck'>JOIN THE ENTIRE BANKLESS COMMUNITY ON FRIDAYS AT 4PM UTC</p> 
                 </Grid>
-                <Grid item xs={12} md={12} style={{minHeight: '500px'}} >
+                <Grid item xs={12} md={12} className='textWreck centered'>
+                    <p>Community Calls are the anchor of the BanklessDAO community. Every Friday at 4pm UTC we gather 
+                        for one hour on the Community Call Voice Stage to hear key governance updates, share hype news, 
+                        and have roundtable discussions on important topics in the DAO. For those unable to join our Discord, 
+                        catch the live stream on 
+                    <a href="https://www.twitch.tv/banklessdao" target="_blank" rel="noreferrer" className='destyle' style={{color: '#f00'}}> BanklessDAO Twitch.</a></p>
                 </Grid>
+
+                <Grid item xs={12} md={6}>
+                    <img src={process.env.PUBLIC_URL + '/assets/media/instructor.png'} alt="instructor" style={{maxWidth: '100%'}} />
+                </Grid>
+
+                <Grid item xs={12} md={6} style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+                    <h1>WANT ALPHA?</h1>
+                    <p style={{paddingTop: '20px'}}>Join the weekly CCs!</p>
+                </Grid>
+
+                <Grid item xs={12} md={12}>
+                    <div className='ccformatBG'>
+                        <Grid container>
+                            <Grid item xs={12} md={4}>
+                                <img src={process.env.PUBLIC_URL + '/assets/media/Dev-Guild-banner.png'} alt="Dev guild" style={{maxWidth: '100%'}} />
+                            </Grid>
+                            <Grid item xs={12} md={8} style={{padding: '20px'}}>
+                                <h1>The CC Format</h1>
+                                <ul>
+                                    <li>0-10 minutes: Moderator opens with key governance updates.</li>
+                                    <li>10-30 minutes: Hype News from the community. What’s the most exciting thing happening in the DAO, and how can we celebrate it?</li>
+                                    <li>30-55 minutes: Roundtable discussion on key topics in the DAO, especially governance.</li>
+                                    <li>55-60 minutes: Moderator closes.</li>
+                                </ul>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Grid>
+
+
+                            
+                <Grid item xs={12} md={6} style={{textAlign: 'center', padding: '20px'}}>
+                    <img src={process.env.PUBLIC_URL + '/assets/media/weeklyupdates.png'} alt="Dev guild" style={{maxWidth: '100%'}} />
+                </Grid>
+                <Grid item xs={12} md={6} style={{display: 'flex', flexDirection: 'column'}}>
+                    <h1>Weekly Updates</h1>
+                    <p>There’s so much going on in the DAO that we have two newsletters designed to share critical updates without crowding the discussions happening in the call. Get up to speed by reading:</p>
+                    <ul>
+                        <li>The Weekly Rollup Newsletter released Fridays.</li>
+                        <li>The Gearing Up Newsletter released Tuesdays.</li>
+                    </ul>
+                </Grid>
+
+                <Grid item xs={12} md={8} className='textWreck' style={{display: 'flex', flexDirection: 'column'}}>
+                    <h1>Evolution</h1>
+                    <p>The CC format was designed by the community via forum discussions and continues to evolve. 
+                        <a href="https://twitter.com/samanthajmarin" target="_blank" rel="noreferrer" className='destyle' style={{color: '#fff'}}> Samanthaj</a> and 
+                        <a href="https://twitter.com/frogmonkee" target="_blank" rel="noreferrer" className='destyle' style={{color: '#fff'}}> frogmonkee</a> led the effort in Season 3 and proposed several changes to revamp the CC format, which used to be a string of updates from coordinators. Read more about the evolution:</p>
+                    <ul>
+                        <li>CC Revamp Draft 1</li>
+                        <li>CC Revamp Draft 2</li>
+                        <li>Gearing Up Newsletter Proposal</li>
+                    </ul>
+                </Grid>
+
+                <Grid item xs={12} md={4} >
+                    <div style={{textAlign:'center', padding: '20px'}}>
+                    <img src={process.env.PUBLIC_URL + '/assets/media/bell-01.png'} alt="bell" style={{maxWidth: '100%', textAlign: 'center'}} />
+                    </div>
+                    <p style={{textAlign:'center'}}>Add the Weekly CC to your calendar!</p>
+                    <div style={{textAlign:'center'}}>
+                    <Button variant="contained" color="error">Add to Calendar</Button>
+                    </div>
+                </Grid>
+
             </Grid>
-        </div>
+        </Container>
     )
 }
