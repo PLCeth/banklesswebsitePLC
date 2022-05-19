c2cbc46a5c4b49109a57243082cd274d7a9785da
PLCeth <dev@bulltrader.io>
2022-05-19 09:54:39 -0500

    cleaning

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,6 +7,23 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 import { Arrow } from '../Utils';
 
+class BoxForPage {
+    constructor(page, desc, image, link) {
+        this.page = page;
+        this.desc = desc;
+        this.image = image;
+        this.link = link;
+    }
+}
+
+const Boxes = [
+    new BoxForPage("MEDIA", "Producing cutting-edge written, visual, and audio content", "/assets/media/media.webp", "#"),
+    new BoxForPage("CONSULTING", "Providing consulting services for other DAOs and Web3-native companies.", "/assets/media/consulting.webp", "#"),
+    new BoxForPage("DAO TOOLING", "Shipping products and services that are changing the future of work.", "/assets/media/dao-tooling.webp", "#"),
+    new BoxForPage("COMING HOME", "Creating a social homebase for artists, thought-leaders, builders, scholars, and innovators in Web3.", "/assets/media/coming-home.webp", "#"),
+]
+
+
 export const HomePage = (props) => {
     return (
         <div style={{paddingBottom: '100px'}}>
@@ -31,97 +48,35 @@ export const HomePage = (props) => {
             <Container>
                 <Grid container>
                     <Grid item xs={12} md={12} style={{paddingTop: '50px'}}>
-
                         <Slide bottom>
                         <h2 style={{textTransform: 'uppercase', textAlign: 'center'}}>We Take a Decentralized approach To:</h2>
                         <div className='divider'></div>
-
                         </Slide>
                     </Grid>
 
-                    <Grid item xs={12} md={6}>
-                        <Fade>
-                        <div className="gridBox">
-                            <img src={process.env.PUBLIC_URL + '/assets/media/media.webp'} alt="Media" className='gridImage'/>
-                            <div className="paddedBox">
-                                <h2 className='subHeader'>Media</h2>
-                                <p style={{padding: '20px', fontSize: '1.2em', color: '#bbb'}}>
-                                    Producing cutting-edge written, visual, and audio content
-                                </p>
-                                <div className="smallDivider"></div>
-                                <div style={{paddingTop: '15px', paddingLeft: '20px'}}>
-                                <span style={{padding: '10px', paddingTop: '25px', borderBottom: '1px solid #222'}}>
-                                    <a href={'#k'} style={{color: '#FFF', textDecoration: 'none'}}>Start <Arrow /></a>
-                                </span>
+                    { Boxes.map((box => {
+                        return (
+                            <Grid item xs={12} md={6}>
+                            <Fade>
+                            <div className="gridBox">
+                                <img src={process.env.PUBLIC_URL + box.image} alt={box.page} className='gridImage'/>
+                                <div className="paddedBox">
+                                    <h2 className='subHeader'>Media</h2>
+                                    <p style={{padding: '20px', fontSize: '1.2em', color: '#bbb'}}>
+                                        { box.desc }
+                                    </p>
+                                    <div className="smallDivider"></div>
+                                    <div style={{paddingTop: '15px', paddingLeft: '20px'}}>
+                                    <span style={{padding: '10px', paddingTop: '25px', borderBottom: '1px solid #222'}}>
+                                        <a href={box.link} style={{color: '#FFF', textDecoration: 'none'}}>Start <Arrow /></a>
+                                    </span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        </Fade>
-                    </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Fade>
-                        <div className="gridBox">
-                            <img src={process.env.PUBLIC_URL + '/assets/media/consulting.webp'} alt="Consulting" className='gridImage'/>
-                            <div className="paddedBox">
-                                <h2 className='subHeader'>Consulting</h2>
-                                <p style={{padding: '20px', fontSize: '1.2em', color: '#bbb'}}>
-                                    Providing consulting services for other DAOs and Web3-native companies.
-                                </p>
-                                <div className="smallDivider"></div>
-
-                                <div style={{paddingTop: '15px', paddingLeft: '20px'}}>
-                                <span style={{padding: '10px', paddingTop: '25px', borderBottom: '1px solid #222'}}>
-                                    <a href={'#k'} style={{color: '#FFF', textDecoration: 'none'}}>Start <Arrow /></a>
-                                </span>
-                                </div> 
-                            </div>
-                        </div>
-                        </Fade>
-                    </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Fade>
-                        <div className="gridBox">
-                            <img src={process.env.PUBLIC_URL + '/assets/media/dao-tooling.webp'} alt="DAO Tooling" className='gridImage'/>
-                            <div className="paddedBox">
-                                <h2 className='subHeader'>DAO Tooling</h2>
-                                <p style={{padding: '20px', fontSize: '1.2em', color: '#bbb'}}>
-                                    Shipping products and services that are changing the future of work.
-                                </p>
-                                <div className="smallDivider"></div>
-
-                                <div style={{paddingTop: '15px', paddingLeft: '20px'}}>
-                                <span style={{padding: '10px', paddingTop: '25px', borderBottom: '1px solid #222'}}>
-                                    <a href={'#k'} style={{color: '#FFF', textDecoration: 'none'}}>Start <Arrow /></a>
-                                </span>
-                                </div> 
-                            </div>
-                        </div>
-                        </Fade>
-                    </Grid>
-
-
-                    <Grid item xs={12} md={6}>
-                        <Fade>
-                        <div className="gridBox">
-                            <img src={process.env.PUBLIC_URL + '/assets/media/coming-home.webp'} alt="Coming Home" className='gridImage'/>
-                            <div className="paddedBox">
-                                <h2 className='subHeader'>Coming Home</h2>
-                                <p style={{padding: '20px', fontSize: '1.2em', color: '#bbb'}}>
-                                    Creating a social homebase for artists, thought-leaders, builders, scholars, and innovators in Web3.
-                                </p>
-                                <div className="smallDivider"></div>
-
-                                <div style={{paddingTop: '15px', paddingLeft: '20px'}}>
-                                <span style={{padding: '10px', paddingTop: '25px', borderBottom: '1px solid #222'}}>
-                                    <a href={'#k'} style={{color: '#FFF', textDecoration: 'none'}}>Start <Arrow /></a>
-                                </span>
-                                </div> 
-                            </div>
-                        </div>
-                        </Fade>
-                    </Grid>
+                            </Fade>
+                        </Grid>
+                        )
+                    }))}
 
                     <Grid item xs={12} md={12} style={{paddingTop: '50px'}}>
                         <h1 style={{textTransform: 'uppercase', textAlign: 'center'}}>Community-Owned and Operated through the:</h1>
@@ -132,7 +87,6 @@ export const HomePage = (props) => {
             </Container>
 
             <Grid container>
-
                 <Grid item xs={12} md={6} style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'column', textAlign: 'center', alignItems: 'center'}}>
                     <img src={process.env.PUBLIC_URL + '/assets/media/bankless-coin.webp'} alt="Bankless Coin" style={{maxWidth: '250px'}}/>
                     <h2 style={{paddingTop: '20px'}}>BANK Token</h2>
